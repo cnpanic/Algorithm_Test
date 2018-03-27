58224b42308cd17845f18a869bfe1a35a4fb1f93
Chu <cnpanic@naver.com>
2018-03-27 19:02:10 +0900

    아스키 + 문자열 위치값 활용 문제 완료

---
+++ Algorithm_Test/count_10.cpp
@@ -0,0 +1,54 @@
+////
+////  count_10.cpp
+////  Algorithm_Test
+////
+////  Created by Jung Ho Chu on 2018. 3. 27..
+////  Copyright © 2018년 Jung Ho Chu. All rights reserved.
+////
+//
+//
+////
+////알파벳 소문자와 대문자로만 이루어진 길이가 N인 단어가 주어진다.
+////
+////한 줄에 10글자씩 끊어서 출력하는 프로그램을 작성하시오.
+////입력
+////
+////첫째 줄에 단어가 주어진다. 단어는 알파벳 소문자와 대문자로만 이루어져 있으며, 길이는 100을 넘지 않는다. 길이가 0인 단어는 주어지지 않는다.
+////출력
+////
+////입력으로 주어진 단어를 열 개씩 끊어서 한 줄에 하나씩 출력한다. 단어의 길이가 10의 배수가 아닌 경우에는 마지막 줄에는 10개 이하의 글자만 출력할 수도 있다.
+//
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string a;
+//    cin >> a;
+//    if(a.size() > 10)
+//    {
+//        cout << a.begin();
+//    }
+//    
+//    
+//} 
+////
+////#include <vector>
+////using namespace std;
+////
+////int main(){
+////
+////    vector<int> v;
+////    v.push_back(4);
+////    v.push_back(9);
+////    v.push_back(17);
+////
+////    vector<int>::iterator p;
+////    for(p=v.begin(); p!=v.end(); p++)
+////        cout << *p << endl;
+////
+////    return 0;
+////}
+//
